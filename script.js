3b7d2b24269ebad13d777206f3e6e61d765a0ab2
Leticia Rocha <maarialeticiashalon3@gmail.com>
2023-11-15 10:52:40 -0300

    atividade javascrip 01

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Cadastro 0 */
+
+let nome = "Fernanda";
+let salario = 1500;
+let idade = 18;
+let possuiDiploma = true;
+
+document.write("Nome: " + nome + "<br>");
+document.write("Salário: " + salario + "<br>");
+document.write("Idade: " + idade + "<br>");
+document.write("Possui diploma? " + possuiDiploma + "<br><br>");
+
+
+/* Cadastro 1 */
+
+let nome1 = "Chico";
+let salario1 = 3000;
+let idade1 = 52;
+let possuiDiploma1 = false;
+
+document.write("Nome: " + nome1 + "<br>");
+document.write("Salário: " + salario1 + "<br>");
+document.write("Idade: " + idade1 + "<br>");
+document.write("Possui diploma? " + possuiDiploma1);
